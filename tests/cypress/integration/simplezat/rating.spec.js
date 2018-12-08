d0b97470429db8f29ef6ba3dee1f7e23de802ae9
zkan <kan@prontomarketing.com>
2018-12-08 12:18:34 +0700

    Add wait time

--- tests/cypress/integration/simplezat/rating.spec.js
+++ tests/cypress/integration/simplezat/rating.spec.js
@@ -10,10 +10,12 @@ context('Rating', () => {
     cy.get('img').should('have.attr', 'alt', 'Negative')
 
     cy.get('img[alt="Positive"]').click()
+    cy.wait(1000)
 
     cy.contains('Any comment?')
     cy.get('input[name="comment"]').type('You are doing great!')
     cy.get('button').click()
+    cy.wait(1000)
 
     cy.contains('Thank you!')
   })
