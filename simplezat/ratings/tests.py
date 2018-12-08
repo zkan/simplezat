06ead7c0a7b1f0c148492685d591b5a17fbdd6ad
zkan <kan@prontomarketing.com>
2018-12-08 17:26:21 +0700

    Use url template tag :bear:

--- simplezat/ratings/tests.py
+++ simplezat/ratings/tests.py
@@ -15,13 +15,18 @@ class RatingViewTest(TestCase):
     def test_rating_view_should_show_three_ratings(self):
         response = self.client.get(self.url)
 
-        expected = '<a href="positive/">' \
+        positive_url = reverse('comments', kwargs={'rating': 'positive'})
+        expected = f'<a href="{positive_url}">' \
             '<img src="/static/images/positive.png" alt="Positive"></a>'
         self.assertContains(response, expected, status_code=200)
-        expected = '<a href="neutral/">' \
+
+        neutral_url = reverse('comments', kwargs={'rating': 'neutral'})
+        expected = f'<a href="{neutral_url}">' \
             '<img src="/static/images/neutral.png" alt="Neutral"></a>'
         self.assertContains(response, expected, status_code=200)
-        expected = '<a href="negative/">' \
+
+        negative_url = reverse('comments', kwargs={'rating': 'negative'})
+        expected = f'<a href="{negative_url}">' \
             '<img src="/static/images/negative.png" alt="Negative"></a>'
         self.assertContains(response, expected, status_code=200)
 
