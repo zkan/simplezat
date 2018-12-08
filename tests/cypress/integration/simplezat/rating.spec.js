32964267364fa37d3d7f103b4f0a2578da998fc1
zkan <kan@prontomarketing.com>
2018-12-08 12:15:57 +0700

    Write test case for positive rating with comment :bear:

---
+++ tests/cypress/integration/simplezat/rating.spec.js
@@ -0,0 +1,20 @@
+context('Rating', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8000')
+  })
+
+  it('should be able to give positve rating with comment', () => {
+    cy.contains('How do we do?')
+    cy.get('img').should('have.attr', 'alt', 'Postive')
+    cy.get('img').should('have.attr', 'alt', 'Neutral')
+    cy.get('img').should('have.attr', 'alt', 'Negative')
+
+    cy.get('img[alt="Positive"]').click()
+
+    cy.contains('Any comment?')
+    cy.get('input[name="comment"]').type('You are doing great!')
+    cy.get('button').click()
+
+    cy.contains('Thank you!')
+  })
+})
